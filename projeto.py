7b34c8885beb4e3319f43c406d8659651363206f
theBatista <filho.antonio@fgv.edu.br>
2025-08-13 11:48:18 -0300

    sorte e calcular media

--- projeto.py
+++ projeto.py
@@ -1,4 +1,15 @@
+import random
+
 def cumprimento(texto):
     return f"Olá, {texto}"
 
 print(cumprimento("Antonio Francisco Batista Filho"))
+
+def sortear_e_calcular_media():
+  nums = random.sample(range(1, 101), 7)
+  soma = sum(nums)
+  media = soma / 7
+  
+  return media
+
+print(sortear_e_calcular_media())
